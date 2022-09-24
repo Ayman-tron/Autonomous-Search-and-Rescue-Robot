d8ac40677d1e43695160347368ca20cab77cde98
Ayman-tron <ayman.abdulkadir@ontariotechu.net>
2022-09-23 22:03:36 -0400

    Testing

--- ir.py
+++ ir.py
@@ -4,13 +4,25 @@ import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BOARD)
 
 GPIO.setup(21, GPIO.IN)
+obstacle_detected = False
 
-
+def obstacle():	
 while True:
     sensor = GPIO.input(21)
     # No obstacle
     if sensor == 1:
-        print("Nothing")
-        time.sleep(0.1)
+       obstacle_detected = False
+       return obstacle_detected
+       time.sleep(0.1)
+
+    # The IR sensor has been configured to detect any obstacle less than or equal to 5cm
     elif sensor == 0:
-        print("Something")
+        obstacle_detected = True
+        return obstacle_detected
+        time.sleep(0.1)
+
+
+if obstacle_detected:
+	print("An obstacle is present")
+else:
+	print("No obstacle")
