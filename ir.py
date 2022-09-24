9e015e414bc62393952e96d2f9b9df1e1b163d27
Ayman-tron <ayman.abdulkadir@ontariotechu.net>
2022-09-23 22:09:47 -0400

    testing

--- ir.py
+++ ir.py
@@ -8,23 +8,23 @@ obstacle_detected = False
 
 
 def obstacle():
-    while True:
-        sensor = GPIO.input(21)
-        # No obstacle
-        if sensor == 1:
-            obstacle_detected = False
-            time.sleep(0.1)
-            return obstacle_detected
+    sensor = GPIO.input(21)
+    # No obstacle
+    if sensor == 1:
+        obstacle_detected = False
+        time.sleep(0.1)
+        return obstacle_detected
 
-        # The IR sensor has been configured to detect any obstacle less than or equal to 5cm
-        elif sensor == 0:
-            obstacle_detected = True
-            time.sleep(0.1)
-            return obstacle_detected
+    # The IR sensor has been configured to detect any obstacle less than or equal to 5cm
+    elif sensor == 0:
+        obstacle_detected = True
+        time.sleep(0.1)
+        return obstacle_detected
 
 
-x = obstacle()
-if x:
-    print("An obstacle is present")
-else:
-    print("No obstacle")
+while True:
+    x = obstacle()
+    if x:
+        print("An obstacle is present")
+    else:
+        print("No obstacle")
