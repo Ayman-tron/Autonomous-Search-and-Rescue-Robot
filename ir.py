eb17753e12c23ea577f600d0c508bde14b4e3385
Ayman-tron <ayman.abdulkadir@ontariotechu.net>
2022-09-24 14:42:15 -0400

    ir.py refactored, pull up resistor added

--- ir.py
+++ ir.py
@@ -3,27 +3,20 @@ import RPi.GPIO as GPIO
 from definitions import *
 
 GPIO.setmode(GPIO.BOARD)
+# In case IR Pin 21 is disconnected the internal resistor of the Pi will ensure that the value does not 
+# fluctuate between 1 and 0. In this case we are using a pull up resistor which means when the wire is disconnected
+# Pin 21 will read a 1
+GPIO.setup(IR_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-GPIO.setup(IR_PIN, GPIO.IN)
-obstacle_detected = False
 
-
+# The IR sensor has been configured to detect any obstacle less than or equal to 5cm
 def obstacle():
     readVal = GPIO.input(IR_PIN)
-    # No obstacle
-    if readVal == 1:
-        time.sleep(0.1)
-        return readVal
+    time.sleep(0.1)
+    return readVal
 
-    # The IR sensor has been configured to detect any obstacle less than or equal to 5cm
-    elif readVal == 0:
-        time.sleep(0.1)
-        return readVal
-
-
-# while True:
-#    x = obstacle()
-#    if x:
-#        print("An obstacle is present")
-#    else:
-#        print("No obstacle")
+# 0 means there is an obstacle, while 1 means no obstacle
+while True:
+    x = obstacle()
+    print(x)
+GPIO.cleanup()
