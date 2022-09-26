9e92a4d9bc8f2f31ab83fb76bee8c333d90dac63
Ayman-tron <ayman.abdulkadir@ontariotechu.net>
2022-09-25 20:56:59 -0400

    accelerometer.py added

---
+++ accelerometer.py
@@ -0,0 +1,20 @@
+from mpu6050 import mpu6050
+import time
+# the address of this sensor is at memory location 68
+mpu = mpu6050(0x68)
+
+while True:
+    # Acceleration data
+    accel_data = mpu.get_accel_data()
+    print("Acc X : "+str(accel_data['x']))
+    print("Acc Y : "+str(accel_data['y']))
+    print("Acc Z : "+str(accel_data['z']))
+    print()
+
+    gyro_data = mpu.get_gyro_data()
+    print("Gyro X : "+str(gyro_data['x']))
+    print("Gyro Y : "+str(gyro_data['y']))
+    print("Gyro Z : "+str(gyro_data['z']))
+    print()
+    print("-------------------------------")
+    time.sleep(1)
